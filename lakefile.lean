fb187f5b101924acb76cb2e6d6b13cb8de107e2f
Matej Penciak <96667244+mpenciak@users.noreply.github.com>
2023-03-30 10:27:48 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -7,10 +7,10 @@ package Nova
 lean_lib Nova
 
 require YatimaStdLib from git
-  "https://github.com/lurk-lab/YatimaStdLib.lean" @ "533d71efa5853ff014f42c174d6321d74251209f"
+  "https://github.com/mpenciak/YatimaStdLib.lean" @ "533d71efa5853ff014f42c174d6321d74251209f"
 
 require BellaNova from git
-  "https://github.com/lurk-lab/Bellanova.lean" @ "885f12bfb39620c8130498925ed948533650cbb3"
+  "https://github.com/mpenciak/Bellanova.lean" @ "885f12bfb39620c8130498925ed948533650cbb3"
 
 require FFaCiL from git
-  "https://github.com/lurk-lab/FFaCiL.lean" @ "9a4c77f8b703b60ae4e353b957e858a2e1de2e83"
+  "https://github.com/mpenciak/FFaCiL.lean" @ "9a4c77f8b703b60ae4e353b957e858a2e1de2e83"
